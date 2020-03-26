b8098bbd8b1421d92b9fb5e81d63188fa75a762c
SeaDamm <SeaDamm@users.noreply.github.com>
2020-03-26 15:09:55 +0100

    Fikset navigation bar

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 //function that makes content of elements visible
 //is being called by button onclick in html
+
 function viseInnhold(innhold) {
-  document.getElementById(innhold).style.display = 'block';
-  
+  var contents = document.getElementById("noe").children
+  for(var node in contents) {
+    contents[node].className = "hidden"
+    if(contents[node].id == innhold) {
+      contents[node].className = "visible";
+    }
   }
+}
